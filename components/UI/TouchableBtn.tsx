0bebfe0881c9ef8928635f1b9b6e77ad4f218273
Ayodeji Afolabi <ayodejigreatest@gmail.com>
2022-04-05 06:31:46 +0100

    Fixed button

--- components/UI/TouchableBtn.tsx
+++ components/UI/TouchableBtn.tsx
@@ -1,9 +1,9 @@
-import { Platform, TouchableNativeFeedback, TouchableOpacity } from 'react-native'
+import { Platform, Pressable, TouchableOpacity } from 'react-native'
 import {ElementType} from 'react'
 
 const TouchableCmp: ElementType = 
     Platform.OS === 'android' && Platform.Version >= 21 ? 
-        TouchableNativeFeedback 
+        Pressable 
         : TouchableOpacity;
 
 export default TouchableCmp
